dc829199e7193de2a37e594c3f0124e33ae019ec
susmitha1509 <71434587+susmitha1509@users.noreply.github.com>
2021-02-11 21:03:13 +0530

    Add files via upload

---
+++ STring.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class STring
+{
+public static void main(String[]args)
+{
+String s1,s2="umadevi";
+Scanner x=new Scanner(System.in);
+System.out.println("enter u r name");
+s1=x.nextLine();
+System.out.println(s1+"  "+s2);
+System.out.println("enter a value");
+int a=x.nextInt();
+System.out.println(Math.sqrt(a));
+}
+}
