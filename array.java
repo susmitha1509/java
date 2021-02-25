ed7a6974eaf7d7a4524daf3d9a81a00a57de0f01
susmitha1509 <71434587+susmitha1509@users.noreply.github.com>
2021-02-25 19:55:22 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class array
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a[]= new int[5];
+System.out.println("enter array elements");
+for(int i=0;i<5;i++)
+{
+a[i]=s.nextInt();
+}
+for(int i=0;i<5;i++)
+{
+System.out.println(a[i]);
+}
+}}
